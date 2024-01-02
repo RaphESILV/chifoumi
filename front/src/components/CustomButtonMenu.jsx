18c563a29bea40bfa37c72fd3fc97ada6cb8d365
Gottax <gauthier.tsn@gmail.com>
2024-01-02 20:25:02 +0100

    update

--- front/src/components/CustomButtonMenu.jsx
+++ front/src/components/CustomButtonMenu.jsx
@@ -30,6 +30,7 @@ const buttonStyle = {
     cursor: 'pointer',
     transition: 'border-color 0.25s',
     marginTop: '30px',
+    background: isHovered ? 'linear-gradient(135deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)' : 'initial',
     transform: isHovered ? 'scale(1.1)' : 'scale(1)',
 };
 
